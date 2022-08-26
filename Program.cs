2e31bca1d31ee6e5dc2542a683cfb9058f35241d
Dreamy <dreamy26@icloud.com>
2022-08-26 01:14:09 -0400

    build: adds Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using.System:
+using System.Collections.Generic;
 
+namespace CS_Using_HashSet
