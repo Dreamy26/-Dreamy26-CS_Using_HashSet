d36233ee832c7429e5ed3b17874126818aba2035
Dreamy <dreamy26@icloud.com>
2022-08-26 01:36:11 -0400

    build: Adds array of strings and loops through them

--- Program.cs
+++ Program.cs
@@ -1,4 +1,39 @@
-﻿using.System:
+﻿using System;
 using System.Collections.Generic;
 
 namespace CS_Using_HashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using Hashset");
+            string[] names = new string[] // names added to array
+            {
+                   "mahesh",
+
+                "vikram",
+
+                "mahesh",
+
+                "mayur",
+
+                "suprotim",
+
+                "saket",
+
+                "manish"
+            };
+            Console.WriteLine("length of Array " + names.Length); // the number of objects to the array, returns 7
+            Console.WriteLine(); // adds a empty line
+
+            Console.WriteLine("The Data in Array"); // Adds the heading to the array objects
+
+            foreach (var n in names) // looping through the objects of the array 
+            {
+                Console.WriteLine(n); // returns the objects one by one under each other
+            }
+            Console.WriteLine(); // Adds empty line
+        }
+    }
+}
